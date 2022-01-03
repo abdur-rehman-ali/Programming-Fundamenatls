bc48b40d8a9c871646053a249e7e0d1666a01434
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-03 23:41:29 +0500

    Pointer:character pointer

---
+++ 08_Pointers/04_Character_pointer/code1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[10] = {1,2};
+    //In case of integer array,
+    //name of array store address of value at 0 index
+    cout<<"Integer array :" <<arr<<endl;
+
+    char ch[4] = "ali";
+    //In case of character array,
+    //name of array will print entire character
+    cout<<"Character array :" <<ch<<endl;
+
+    char *c = &ch[0];
+    cout<<c<<endl;
+
+    // Not recommended
+    char temp = 'z';
+    char *p = &temp;
+
+    cout << p << endl;
+
+}
