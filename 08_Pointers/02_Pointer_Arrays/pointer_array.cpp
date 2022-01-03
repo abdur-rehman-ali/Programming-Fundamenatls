9e1689e1d7e1200a588a579ebe268b58bb8a5756
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-03 18:46:47 +0500

    Pointers:Array

---
+++ 08_Pointers/02_Pointer_Arrays/pointer_array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[10] = {23, 122, 41, 67};
+
+    cout << "*************************************" << endl
+         << endl;
+    cout << "address of first memory block is " << arr << endl;
+    cout << "value at first block" << arr[0] << endl;
+    cout << "address of first memory block is " << &arr[0] << endl;
+    cout << "4th " << *arr << endl;
+    cout << "5th " << *arr + 1 << endl;
+    cout << "6th " << *(arr + 1) << endl;
+    cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+    cout << "9th " << *(arr + 2) << endl;
+
+    cout << "*************************************" << endl
+         << endl;
+
+    int i = 3;
+    //  i[arr] = * (i+arr)
+    // arr[i] = *(arr+i)
+    cout << i[arr] << endl;
+    cout << "*************************************" << endl
+         << endl;
+
+    int temp[10] = {1, 2};
+    cout << sizeof(temp) << endl;
+    cout << "1st " << sizeof(*temp) << endl;
+    cout << "2nd " << sizeof(&temp) << endl;
+    int *ptr = &temp[0];
+    cout << sizeof(ptr) << endl;
+    cout << sizeof(*ptr) << endl;
+    cout << sizeof(&ptr) << endl;
+    int a[20] = {1, 2, 3, 5};
+    cout << " ->" << &a[0] << endl;
+    cout << &a << endl;
+    cout << a << endl;
+    int *p = &a[0];
+    cout << p << endl;
+    cout << *p << endl;
+    cout << "-> " << &p << endl;
+
+    cout << "*************************************" << endl;
+
+    int arr1[10];
+
+    //ERROR
+    //arr = arr+1;
+
+    int *ptr1 = &arr1[0];
+    cout << ptr1 << endl;
+    ptr1 = ptr1 + 1;
+    cout << ptr1 << endl;
+
+    return 0;
+}
