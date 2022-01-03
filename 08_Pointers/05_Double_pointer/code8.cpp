66673b416d50c6df0282f5f3964a659d7cbc428e
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-04 03:12:13 +0500

    Pointers:Double with function

---
+++ 08_Pointers/05_Double_pointer/code8.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+void update(int **ptr)
+{
+    cout<<"Derefrencing ptr "<<**ptr<<endl; //5
+    *ptr = *ptr+1;
+    cout<<"Derefrencing ptr "<<**ptr<<endl; //Dummy value
+}
+
+int main()
+{
+    int num= 5;
+    int *ptr = &num;
+    int **ptr2 = &ptr;
+
+    cout<<"Derefrencing ptr "<<**ptr2<<endl; //5
+  
+
+    //incrementing value using function
+    cout<<"Going inside function"<<endl<<endl;
+    update(ptr2);
+    cout<<"Came out of function"<<endl<<endl;
+
+    cout<<"Derefrencing ptr "<<**ptr2<<endl; //Dummy value
+ 
+
+
+}
