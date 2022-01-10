9887007e7b9c6ff9286c7b0655684a3695b5c28a
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-10 07:48:52 +0500

    Pointer:MCQ28

---
+++ 08_Pointers/08_MCQS/mcq28.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int first = 110;
+    int *p = &first;
+    int **q = &p;
+    int second = (**q)++ + 9;
+    cout << first << " " << second << endl; 
+}
+
+
+
+//Answer
+//111 119
