9a0d6183332983baf071b331ce8418f877cb04a3
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-10 07:48:02 +0500

    Pointer:MCQ21

---
+++ 08_Pointers/08_MCQS/mcq21.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[]= "joyia";
+    char *p = str;
+    cout << *p << endl;
+    cout << *(p + 1) << endl;
+    cout << *(p + 2) << endl;
+    
+}
+
+//Answer
+// j
+// o
+// y
