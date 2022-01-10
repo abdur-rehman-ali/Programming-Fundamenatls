8ecd3573f8e637750999ef16310feadfe6aae898
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-10 06:52:07 +0500

    Pointer:MCQ13

---
+++ 08_Pointers/08_MCQS/mcq13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {11, 21, 31, 41};
+    //we cant change value store in arr in symbol table
+    // int *ptr = arr++;
+    // cout << *ptr << endl;
+}
+
+//Answer
+//ERROR
