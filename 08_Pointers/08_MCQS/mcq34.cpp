fb784fc06299a15fd2f52649cf3b1ef312fd2dcb
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-10 07:49:38 +0500

    Pointer:MCQ34

---
+++ 08_Pointers/08_MCQS/mcq34.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float arr[5] = {12.5, 10.0, 13.5, 90.5, 0.5};
+    float *ptr1 = &arr[0];
+    float *ptr2 = ptr1 + 4;
+    cout << *ptr2 << " ";
+    cout << ptr2 - ptr1;
+    return 0;
+}
+
+
+//Answer
+//0.5 4
