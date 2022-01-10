6ef8deb8c51e0293931f91775b1f98b8be88740e
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-10 06:33:32 +0500

    Pointer:MCQ10

---
+++ 08_Pointers/08_MCQS/mcq10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {11, 21, 13};
+    cout << (arr ) << endl;
+    cout << (arr + 1) << endl;
+    cout << (arr + 2) << endl;
+}
+
+//Answer
+// 0x61fef8 == 8
+// 0x61fefc == 12
+// 0x61ff00 == 16
