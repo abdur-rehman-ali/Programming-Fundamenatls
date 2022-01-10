af2fe7f55ac3b2084bebacb88d1826937460e734
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-10 07:49:23 +0500

    Pointer:MCQ32

---
+++ 08_Pointers/08_MCQS/mcq32.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char *p;
+    char str[] = "pqrstuv";
+    p = str;
+    p += 3;
+    cout << p;
+    return 0;
+}
+
+//Answer
+//stuv
