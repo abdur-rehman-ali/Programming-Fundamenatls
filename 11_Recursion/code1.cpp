56613c051e8d1f86238c665aea68208a9d9e4eeb
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-19 20:49:54 +0500

    Recursion:code1

--- 11_Recursion/code1.cpp
+++ 11_Recursion/code1.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+//Power of 2 
+
+int powerOfTwo(int num)
+{
+
+    //base case
+    if(num==0)
+    {
+        return 1;
+    }
+
+    //resursive function
+    int subproblem = powerOfTwo(num-1);
+    return 2 * subproblem;
+}
+
 int main()
 {
-    cout<<"Recursion"<<endl;
+    cout<<powerOfTwo(5)<<endl;
+    cout<<powerOfTwo(9)<<endl;
+    cout<<powerOfTwo(4)<<endl;
 }
