e903392f6b44a84dd5c0cf6c18a3558a6ce2eab9
abdur-rehman-ali <alijoyia911@gmail.com>
2022-01-21 14:40:42 +0500

    Sorting:Insertionsort (modified)

--- 10_Sorting/03_Insertion_Sort/code1.cpp
+++ 10_Sorting/03_Insertion_Sort/code1.cpp
@@ -1,39 +1,38 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 // https://www.geeksforgeeks.org/insertion-sort/
 
-void insertionSort(int *arr,int size)
+void insertionSort(int *arr, int size)
 {
-    for(int i=1;i<=size-1;i++)
+    for (int i = 1; i <= size - 1; i++)
     {
-        int temp=arr[i];
-        int j=i-1;
-        while(arr[j]>temp && j>=0)
+        int temp = arr[i];
+        int j = i - 1;
+        while (arr[j] > temp && j >= 0)
         {
-            arr[j+1] = arr[j];
+            arr[j + 1] = arr[j];
             j--;
         }
         arr[++j] = temp;
     }
 }
 
-void printArray(int arr[],int length)
+void printArray(int arr[], int length)
 {
-    for(int i=0;i<length;i++)
+    for (int i = 0; i < length; i++)
     {
-        cout<<arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int main()
 {
-    int arr[] = {2,3,4,5,0,1,23,42,54,43};
-    int length = sizeof(arr)/sizeof(int);
-    printArray(arr,length);
-    insertionSort(arr,length);
-    printArray(arr,length);
-
+    int arr[] = {2, 3, 4, 5, 0, 1, 23, 42, 54, 43};
+    int length = sizeof(arr) / sizeof(int);
+    printArray(arr, length);
+    insertionSort(arr, length);
+    printArray(arr, length);
 }
